a92f5c2ed3b29f0ed2937eaa3994f9c2ca4041ba
ASUS <irfanfauziyk@gmail.com>
2024-03-06 10:46:52 +0700

    membetulkan masukkan elemen array dan jumlah pass

--- PrakLogarithm1/PrakLogarithm1.cpp
+++ PrakLogarithm1/PrakLogarithm1.cpp
@@ -19,9 +19,9 @@ void input() {
         }
     }
     cout << endl;
-    cout << "=====================";
-    cout << "Masukkan Elemen Array";
-    cout << "=====================";
+    cout << "=====================" << endl;
+    cout << "Masukkan Elemen Array" << endl;
+    cout << "=====================" << endl;
 
     for (int i = 0; i < n; i++) {
         cout << " Data ke-" << (i + 1) << ": ";
@@ -33,6 +33,7 @@ void input() {
 
 void bubbleSortArray() {
     int pass = 1; 
+
     do {
         for (int j = 0; j <= n - 1 - pass; j++) {
             if (a[j] > a[j + 1]) {
@@ -49,13 +50,14 @@ void bubbleSortArray() {
 
 void display() {
     cout << endl;
-    cout << "============" << endl;
+    cout << "===================================" << endl;
     cout << "Element Array yang telah tersusun " << endl;
     cout << "===================================" << endl;
     for (int j = 0; j < n; j++) {
-        cout << "Jumalah pass = " << n - 1 << endl;
-        cout << endl;
+        cout << a[j] << endl; 
     }
+    cout << "Jumalah pass = " << n - 1 << endl;
+    cout << endl;
 }
 
 int main(){
@@ -63,7 +65,9 @@ int main(){
     input();
     bubbleSortArray();
     display();
+    system("pause");
 
+    return 0;
     
 }
 
