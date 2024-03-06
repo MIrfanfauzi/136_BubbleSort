66c1dffc91c5bc67884c7b67370b401ff4fb4e1d
ASUS <irfanfauziyk@gmail.com>
2024-03-06 10:09:54 +0700

    input dari pengguna

--- PrakLogarithm1/PrakLogarithm1.cpp
+++ PrakLogarithm1/PrakLogarithm1.cpp
@@ -7,7 +7,29 @@ using namespace std;
 int a[20];
 int n;
 
+void input() {
+    while (true) {
+        cout << " Masukkan banyaknya elemen pada array : ";
+        cin >> n;
+        if (n <= 20)
+            break;
+        else {
+            cout << "\nArray dapat mempunyai maksimal 20 elemen. \n";
 
+        }
+    }
+    cout << endl;
+    cout << "=====================";
+    cout << "Masukkan Elemen Array";
+    cout << "=====================";
+
+    for (int i = 0; i < n; i++) {
+        cout << " Data ke-" << (i + 1) << ": ";
+        cin >> a[i];
+
+    }
+
+}
 
 int main()
 {
